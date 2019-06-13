883473bbd82a5dcc20d9954defca3c5997b1c1ed
Student <example@example.com>
2019-06-12 20:05:40 -0700

    save the stuff

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    $(.favorite-eye-color).append("blah blah");
+    $(.favorite-bird).append("blah blah");
+    $(.favorite-mammal).append("blah blah");
+    $(.favorite-reptile).append("blah blah");
+    $(.favorite-beverage).append("blah blah");
+    $(.favorite-snack).append("blah blah");
+    $(.favorite-dessert).append("blah blah");
+    $(.favorite-bug).append("blah blah");
+    $(.favorite-sport).append("blah blah");
+    $(.favorite-cake).append("blah blah");
+    $(.favorite-pie).append("blah blah");
+    $(.favorite-pizza).append("blah blah");
+    $(.favorite-color).append("blah blah");
+    $(.favorite-brand).append("blah blah");
+    $(.favorite-cartoon).append("blah blah");
+    $(.favorite-movie).append("blah blah");
+    $(.favorite-mythical creature).append("blah blah");
+    event.preventDefault();
+  });
+});
